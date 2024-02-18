cd24be119f1cd616f754d2fc664ebd0dcc8b58c7
FoundationINCCorporateTeam <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-02-17 18:10:48 -0600

    Update Protected.svelte

--- src/routes/Protected.svelte
+++ src/routes/Protected.svelte
@@ -17,6 +17,25 @@
       confirmButtonText: 'Will do!',
     })
   }
+
+  function openIframe(url) {
+    let win = window.open("", "_blank");
+    let iframe = document.createElement("iframe");
+    iframe.src = url;
+    iframe.style.position = "fixed";
+    iframe.style.top = "0";
+    iframe.style.left = "0";
+    iframe.style.bottom = "0";
+    iframe.style.right = "0";
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.border = "none";
+    iframe.style.margin = "0";
+    iframe.style.padding = "0";
+    iframe.style.overflow = "hidden";
+    iframe.style.zIndex = "999999";
+    win?.document.body.appendChild(iframe);
+  }
 </script>
 
 <svelte:head>
@@ -25,4 +44,17 @@
 
 {#if $user && $user.username}
   <h1>Protected</h1>
-{:else}{handlePrivateRoute()}{/if}
+{:else}
+  {handlePrivateRoute()}
+  <div style="text-align: center;">
+    <button class="button-centered" on:click={() => openIframe("https://mnsecurebrowser.spoggi.com")}>
+      CLICK HERE \\ MN Games Premium Website // CLICK HERE
+    </button>
+    <button class="button-centered" on:click={() => openIframe("https://v1-hjrifndbdhjdjdndbdhdhdbvr.manica.org/")}>
+      CLICK HERE \\ MN Games Ultimate v1 Website // CLICK HERE
+    </button>
+    <button class="button-centered" on:click={() => openIframe("https://learnedu2.hillbrick.net")}>
+      CLICK HERE \\ MN Games Ultimate v2 Website // CLICK HERE
+    </button>
+  </div>
+{/if}
